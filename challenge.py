6ce154341cdc9bc732513de00d95e0eaedceffa5
GloopySoup <tayabz7935@gmail.com>
2026-01-17 19:42:17 +0000

    Made program

---
+++ challenge.py
@@ -0,0 +1,34 @@
+string1 = """product_id, product_name, product_price
+1, doom, 10
+2, cake, 25"""
+
+string2 = """team_id, team_name, team_tsales
+3, Hello United, 14
+5, FootballFC, 20
+7, Team, 3"""
+
+def dict_maker(x):
+    list_of_dicts = []
+    x = x.splitlines()
+    hi = 'hi'
+    keys = x[0]  
+    items = x[ 1:len(x)]
+
+
+
+
+
+
+    keys_splitted = keys.split(",")
+
+    list_of_dicts = [dict() for i in range(len(x)-1)]
+    for i  in range(len(items)):
+        values = items[i].split(",")
+        for y in range(len(keys_splitted)):
+            list_of_dicts[i][keys_splitted[y]] = [values[y]]
+
+    return list_of_dicts
+
+print(dict_maker(string1))
+print(dict_maker(string2))
+
